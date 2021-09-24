a29cf007028873631ef953a1a2ed5135385b9d35
siddartha1997 <siddarthanarsepalli@gmail.com>
2021-09-24 12:27:24 +0530

    homepage commit

---
+++ src/pages/Product.js
@@ -0,0 +1,32 @@
+import React from "react";
+import ReactStars from "react-rating-stars-component";
+//import { Link, useHistory } from "react-router-dom";
+
+function Product({ productDetails }) {
+  //let history = useHistory();
+  const handleClick = () => {
+    //history.push("/dashboard");
+  };
+  return (
+    <div className="container" onClick={handleClick}>
+      <img src={productDetails.icon}></img>
+      <div className="flex-container">
+        <div className="product-name">{productDetails.appName}</div>
+        <div>{productDetails.publisher.name}</div>
+        <div style={{ display: "flex", alignItems: "center",marginBottom:'10px' }} className="rating">
+          <ReactStars
+            count={5}
+            size={24}
+            isHalf={true}
+            value={productDetails.avgRating}
+            edit={false}
+            activeColor="#ffd700"
+          />
+          <div style={{ marginTop: "6px" }}>{productDetails.avgRating}</div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Product;
